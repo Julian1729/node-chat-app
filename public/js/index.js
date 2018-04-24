d2bf5ac0dc4193d9780ecc847777ba5d6423ec29
Julian1729 <hernandez.julian17@gmail.com>
2018-04-24 19:26:43 -0400

    Add geolocation support via geolocation api

--- public/js/index.js
+++ public/js/index.js
@@ -16,6 +16,16 @@ socket.on('disconnect', function(){
   console.log('disconnected from server');
 });
 
+socket.on('newLocationMessage', function(message){
+  console.log('loca message');
+  var li = $('<li></li>');
+  var a = $('<a target="_blank">My Current Location</a>');
+  li.text(`${message.from}: `);
+  a.attr('href', message.url);
+  li.append(a);
+  $('#messages').append(li);
+});
+
 $('#message-form').on('submit', function(e){
   e.preventDefault();
   socket.emit('createMessage', {
@@ -23,5 +33,21 @@ $('#message-form').on('submit', function(e){
     text: $('#message-form [name=message]').val()
   }, function(){
 
+  });
+});
+
+var locationButton = $('#send-location');
+locationButton.on('click', function(){
+  if(!navigator.geolocation){
+    return alert('Geolocation not supported by your browser');
+  }
+  navigator.geolocation.getCurrentPosition(function(position){
+    socket.emit('createLocationMessage', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    });
+  }, function(){
+    // if user presses deny
+    alert('Unable to fetch location.')
   });
 });
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 const expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generate message', () => {
 
@@ -12,6 +12,22 @@ describe('generate message', () => {
 
     expect(msg.from).toBe(from);
     expect(msg.text).toBe(text);
+    expect(typeof msg.createdAt).toBe('number');
+
+  });
+
+});
+
+describe('generateLocationMessage', () => {
+
+  it('should generate correct location object', () => {
+
+    var from = 'admin';
+    var lat = '40.0233916';
+    var long = '-75.12139690000001';
+
+    var msg = generateLocationMessage(from, lat, long);
+    expect(msg.url).toBe(`https://www.google.com/maps?q=${lat},${long}`);
     expect(typeof msg.createdAt).toBe('number');
 
   });
